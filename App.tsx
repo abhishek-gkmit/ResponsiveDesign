6ae82263432634a6e50d8c54132e4101ada1ca31
Abhishek <abhishek@gkmit.co>
2024-11-18 16:20:39 +0530

    fix: add suggested changes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
-import {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
-import SplashScreen from '@screens/splash';
 
 import {ThemeContextProvider} from '@config/contexts/ThemeContext';
 import MainNavigator from '@config/MainNavigator';
 
 function App() {
-  const [showSplashScreen, setShowSplashScreen] = useState(true);
-
   return (
     <ThemeContextProvider>
       <NavigationContainer>
-        {showSplashScreen ? (
-          <SplashScreen hideSplashScreen={() => setShowSplashScreen(false)} />
-        ) : (
-          <MainNavigator />
-        )}
+        <MainNavigator />
       </NavigationContainer>
     </ThemeContextProvider>
   );
--- src/config/MainNavigator.tsx
+++ src/config/MainNavigator.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import Login from '@screens/login';
+import SplashScreen from '@screens/splash';
+
 import StackNavigator from '@navigation/StackNavigator';
 
 function MainNavigator() {
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
-  return isUserLoggedIn ? (
-    <StackNavigator />
+  const navigator = useMemo(() => {
+    return isUserLoggedIn ? (
+      <StackNavigator />
+    ) : (
+      <Login makeUserLogin={() => setIsUserLoggedIn(true)} />
+    );
+  }, [isUserLoggedIn, setIsUserLoggedIn]);
+
+  return showSplashScreen ? (
+    <SplashScreen hideSplashScreen={() => setShowSplashScreen(false)} />
   ) : (
-    <Login makeUserLogin={() => setIsUserLoggedIn(true)} />
+    navigator
   );
 }
 
